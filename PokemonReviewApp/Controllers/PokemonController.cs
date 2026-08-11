476a04921bbcff765ed2b9cce94f931783a161c0
Eren Gündoğmuş <erengundogmus5@gmail.com>
2026-08-11 10:22:43 +0300

    pokemon için interface,repo ve controller taslaklarını oluşturdum

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonInterface pokemonInterface;
+
+        public PokemonController(IPokemonInterface pokemonInterface)
+        {
+            this.pokemonInterface = pokemonInterface;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+
+        public IActionResult GetPokemons()
+        {
+            var pokemons = pokemonInterface.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonInterface
+    {
+        private readonly DataContext context;
+
+        public PokemonRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+             return this.context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
