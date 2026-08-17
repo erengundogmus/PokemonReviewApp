e98a7671707ca0fdbc037eb3d2271e4dcff14bea
Eren Gündoğmuş <erengundogmus5@gmail.com>
2026-08-17 10:38:49 +0300

    Update initial structure with try-catch usage

--- PokemonConsoleApp/Program.cs
+++ PokemonConsoleApp/Program.cs
@@ -1,31 +1,37 @@
-﻿namespace PokemonConsoleApp
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
+namespace PokemonConsoleApp
+{
     class Program
     {
         static readonly string baseUrl = "https://localhost:7013/api/";
+        static readonly HttpClient client = new HttpClient();
 
         static async Task Main(string[] args)
         {
             bool exit = false;
 
-            while (exit)
+            while (!exit)
             {
                 Console.Clear();
                 Console.WriteLine("//Ana Menü");
                 Console.WriteLine();
-                Console.WriteLine("1-Pokemon");
-                Console.WriteLine("2-Category");
-                Console.WriteLine("3-Country");
-                Console.WriteLine("4-Food");
-                Console.WriteLine("5-Owner");
-                Console.WriteLine("6-Review");
-                Console.WriteLine("7-Reviewer");
-                Console.WriteLine("0-Çıkış");
+                Console.WriteLine("1 - Category");
+                Console.WriteLine("2 - Country");
+                Console.WriteLine("3 - Food");
+                Console.WriteLine("4 - Owner");
+                Console.WriteLine("5 - Pokemon");
+                Console.WriteLine("6 - Review");
+                Console.WriteLine("7 - Reviewer");
+                Console.WriteLine("0 - Exit");
                 Console.WriteLine();
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine();
-                Console.Write("Gitmek istediğiniz menüyü seçin: ");
+                Console.Write("Please choose the menu you want to continue:");
 
                 string choice = Console.ReadLine();
 
@@ -34,38 +40,39 @@
                     //case "1":
                     //    await CategoryMenu();
                     //    break;
-
+                    
                     //case "2":
                     //    await CountryMenu();
                     //    break;
-
+                   
                     //case "3":
                     //    await FoodMenu();
                     //    break;
-
+                  
                     //case "4":
                     //    await OwnerMenu();
                     //    break;
-
+                  
                     case "5":
                         await PokemonMenu();
                         break;
-
+                    
                     //case "6":
                     //    await ReviewMenu();
                     //    break;
-
+                    
                     //case "7":
                     //    await ReviewerMenu();
                     //    break;
-
+                    
                     case "0":
                         exit = true;
-                        Console.WriteLine("Çıkış yapılıyor");
+                        Console.WriteLine("See you next time. :)");
                         break;
-
-                    default: Console.WriteLine("Seçim geçerli değil, devam etmek için bir tuşa basın");
-                        Console.Read();
+                    
+                    default:
+                        Console.WriteLine("Invalid choice, press a key to continue.");
+                        Console.ReadKey();
                         break;
                 }
             }
@@ -74,86 +81,93 @@
         static async Task PokemonMenu()
         {
             bool backToMain = false;
+
             while (!backToMain)
             {
                 Console.Clear();
                 Console.WriteLine("//Pokemon");
-                Console.WriteLine("1-GetAll");
-                Console.WriteLine("2-GetById");
-                Console.WriteLine("3-Create");
-                Console.WriteLine("4-Update");
-                Console.WriteLine("5-Delete");
-                Console.WriteLine("0-Ana Menüye Dön");
+                Console.WriteLine("1 - GetAll");
+                Console.WriteLine("2 - GetById");
+                Console.WriteLine("3 - Create");
+                Console.WriteLine("4 - Update");
+                Console.WriteLine("5 - Delete");
+                Console.WriteLine("0 - Exit to Main Menu");
                 Console.WriteLine();
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine();
-                Console.Write("Seçiminiz: ");
+                Console.Write("Please choose the process you want to continue: ");
 
                 string choice = Console.ReadLine();
 
-                switch(choice)
+                switch (choice)
                 {
                     case "1":
                         await GetAllPokemons();
                         break;
-
-                    //case "2":
+                    // case "2":
                     //    await GetPokemonById();
                     //    break;
-                    
-                    //case "3":
-                    //    await CreatePokemon();
-                    //    break;
-                    
-                    //case "4":
-                    //    await UpdatePokemon();
-                    //    break;
-                    
-                    //case "5":
-                    //    await DeletePokemon();
-                    //    break;
-
                     case "0":
                         backToMain = true;
                         break;
-                    default :
-                        Console.WriteLine("Geçersiz seçim, devam edebilmek için herhangi bir tuşa basın");
+                    default:
+                        Console.WriteLine("Invalid choice, press a key to continue.");
                         Console.ReadKey();
                         break;
                 }
             }
-
         }
 
 
         static async Task GetAllPokemons()
         {
+            Console.Clear();
+            Console.WriteLine("//Pokemon List is Loading\n");
 
+            try
+            {
+                //apiye istek atıyoruz
+                HttpResponseMessage response = await client.GetAsync(baseUrl + "Pokemon");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    //json dosyasını dönüştürüyoruz
+                    List<Pokemon> pokemonList = await response.Content.ReadFromJsonAsync<List<Pokemon>>();
+
+                    Console.WriteLine("--- POKEMON LİST ---");
+
+                    //foreach ile döndürüyoruz
+                    if (pokemonList != null && pokemonList.Count > 0)
+                    {
+                        foreach (Pokemon p in pokemonList)
+                        {
+                            Console.WriteLine($"{p.Id} - Name: {p.Name} | Birth Date: {p.BirthDate.ToString("yyyy-MM-dd")}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("There are no pokemons in the database.");
+                    }
+
+                    Console.WriteLine("-----------------------");
+                }
+                else
+                {
+                    Console.WriteLine($"Error: Received an unsuccessful response from the API. Status Code: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Connection Error: Could not reach the API. Please make sure the API is running.");
+                Console.WriteLine($"{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error has occured: {ex.Message}");
+            }
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
